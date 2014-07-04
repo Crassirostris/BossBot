f33fd377c6fd6ea26e457ceaae6c46382d122cac
DenisovAlexander <vallis@olympus.ru>
2014-07-04 15:10:18 +0600

    change destination

--- WinLikeBoss/BossBot/BossBot.cs
+++ WinLikeBoss/BossBot/BossBot.cs
@@ -10,6 +10,7 @@ namespace BossBot
     public class BossBot : AdvancedRobot
     {
         private double direction = 1;
+        private bool directionChanged;
         private long lastTimeScannedCurrentFoe;
         private bool isTurning;
         private bool isMoving;
@@ -110,7 +111,11 @@ namespace BossBot
 
         private void Move()
         {
-            ChangeDirection();
+            if (directionChanged)
+            {
+                direction *= -1;
+                directionChanged = false;
+            }
 
             if (!isTurning && !isMoving)
             {
@@ -133,12 +138,6 @@ namespace BossBot
             }
         }
 
-        private void ChangeDirection()
-        {
-            direction *= -1;
-        }
-
-
         private double GetDistanceToWall()
         {
             var trackDeltas = foes.Keys
@@ -146,14 +145,12 @@ namespace BossBot
                 .Select(name => new DeltaInfo(foes[name], this))
                 .ToList();
 
-            Out.WriteLine(trackDeltas.Count);
-
             if (Heading <= 2 || Heading >= 358)
             {
                 if (trackDeltas.Any(delta => X >= delta.MinX && X <= delta.MaxX && Y <= delta.MinY))
                 {
                     Out.WriteLine("ololo");
-                    return Y - Height/2;
+                    return -Y + Height/2;
                 }
                 if (X >= Width + 2)
                     return BattleFieldHeight - Y - Height/2;
@@ -167,8 +164,8 @@ namespace BossBot
                         Out.WriteLine("ololo");
                     }
                     if (Y >= BattleFieldHeight - Height - 2)
-                        return X - 3 * Width / 2;
-                    return X - Width / 2;
+                        return -(X - 3 * Width / 2);
+                    return -(X - Width / 2);
                 }
                 return BattleFieldWidth - X - Width/2;
             }
@@ -180,15 +177,15 @@ namespace BossBot
                         Out.WriteLine("ololo");
                     }
                     if (X >= Width + 2)
-                        return BattleFieldHeight - Y - Height/2;
-                    return BattleFieldHeight - Y - 3*Height/2;
+                        return -(BattleFieldHeight - Y - Height/2);
+                    return -(BattleFieldHeight - Y - 3*Height/2);
                 }
                 return Y - Height/2;
             }
             if (trackDeltas.Any(delta => Y >= delta.MinY && Y <= delta.MaxY && X >= delta.MaxX))
             {
                 Out.WriteLine("ololo");
-                return BattleFieldWidth - X - Width/2;
+                return -(BattleFieldWidth - X - Width/2);
             }
             if (Y >= BattleFieldHeight - Height - 2)
                 return X - 3*Width/2;
@@ -207,12 +204,12 @@ namespace BossBot
 
         public override void OnHitRobot(HitRobotEvent evnt)
         {
-            ChangeDirection();
+            directionChanged = true;
         }
 
         public override void OnHitByBullet(HitByBulletEvent evnt)
         {
-            isMoving = false;
+            directionChanged = true;
         }
 
         public override void OnBulletHit(BulletHitEvent evnt)
